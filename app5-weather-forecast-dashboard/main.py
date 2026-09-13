b51d225fe3fc390ab4a40e0794e6e7c510ced7b3
mknsrp <krawczykmaciekit@gmail.com>
2026-09-13 13:33:11 +0200

    app5-weather-forecast-dashboard: Plot fake data dynamically

--- app5-weather-forecast-dashboard/main.py
+++ app5-weather-forecast-dashboard/main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+
 st.title("Weather Forecast Dashboard")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days",
@@ -6,3 +8,14 @@ days = st.slider("Forecast Days",
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+
+def get_data(days):
+    dates = ["2022-05-10", "2022-07-01", "2022-12-03"]
+    temperatures = [15, 32, 8]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
