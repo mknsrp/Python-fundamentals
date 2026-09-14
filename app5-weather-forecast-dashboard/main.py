6c94c875fed4857cdd0936003aa1d7a4ba613c61
mknsrp <krawczykmaciekit@gmail.com>
2026-09-14 19:16:02 +0200

    app5-weather-forecast-dashboard: get data about temperatures and sky condition

--- app5-weather-forecast-dashboard/main.py
+++ app5-weather-forecast-dashboard/main.py
@@ -10,9 +10,9 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, displaying_way)
+data = get_data(place, days, kind)
 
-d, t = get_data(days)
+d, t = get_data(place, days, option)
 
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
